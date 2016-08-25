39677d2f51ee8ea89ae9e6069178e6ef4e2e9ee0
hunterowens <owens.hunter@gmail.com>
2016-08-25 15:47:30 -0500

    more work:

--- basic_pipeline.py
+++ basic_pipeline.py
@@ -1,6 +1,6 @@
 import luigi
 import requests
-
+import pandas as pd 
 
 class DownloadTask(luigi.ExternalTask):
     """
@@ -15,6 +15,35 @@ class DownloadTask(luigi.ExternalTask):
     
     def output(self):
         return luigi.LocalTarget("data/permits.csv") 
+
+## TODO ## 
+class MakeDataFrameTask(luigi.Task):
+    "Pandas integration because we real cool"
+    def requires(self):
+        return DownloadTask()
+
+    def run(self):
+        df = pd.read_csv(self.input().open('r'))
+        import ipdb; ipdb.set_trace() 
+    def output(self):
+        pass
+
+## TODO ## 
+class ComputeWards(luigi.Task):
+    pass
+
+## TODO ## 
+class MakePredictions(luigi.Task):
+    pass
+
+## TODO ##
+class UploadPredictions(luigi.Task):
+    pass
+
+## TODO ## 
+class LoadCensus(luigi.Task):
+    pass
+
 
 if __name__ == '__main__':
     luigi.run()
