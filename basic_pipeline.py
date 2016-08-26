e2c67d4fadbc31d79e1929c16b9bbbf14b332c6e
hunterowens <owens.hunter@gmail.com>
2016-08-26 13:08:33 -0500

    last things

--- basic_pipeline.py
+++ basic_pipeline.py
@@ -1,7 +1,7 @@
 import luigi
 import requests
 import pandas as pd 
-
+import geocoder 
 class DownloadTask(luigi.ExternalTask):
     """
     Downloads Permit Data from the Portal
@@ -16,20 +16,32 @@ class DownloadTask(luigi.ExternalTask):
     def output(self):
         return luigi.LocalTarget("data/permits.csv") 
 
+class cleanCSV(luigi.Task):
+    """This is our cleaning step"""
+
+    def requires(self):
+        return DownloadTask
+
+
 ## TODO ## 
-class MakeDataFrameTask(luigi.Task):
+class ReverseGeocode(luigi.Task):
     "Pandas integration because we real cool"
     def requires(self):
         return DownloadTask()
 
     def run(self):
         df = pd.read_csv(self.input().open('r'))
-        import ipdb; ipdb.set_trace() 
+        def reverse 
+    
     def output(self):
         pass
 
 ## TODO ## 
-class ComputeWards(luigi.Task):
+class MakeWards(luigi.Task):
+    pass
+
+## TODO ## 
+class MakeMaps(luigi.Task):
     pass
 
 ## TODO ## 
